eb79d0db9c86341f77e6ddb11d56902d1d16af1d
Rahul Saroj <sarojrahul1399@gmail.com>
2024-08-16 02:51:10 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,38 @@
+import {
+  Chat,
+  DonutLarge,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
